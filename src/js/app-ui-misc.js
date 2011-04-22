f39b2dad7bbb0cb50d383c15a55fef86c34b130f
Benjamin Young <byoung@bigbluehat.com>
2011-04-22 17:06:08 -0400

    properly apply the Apache 2.0 license to JS files
    Change-Id: Ic0f43adc450837d7ab31d642e33e7114a9dedcf3
    Reviewed-on: http://review.membase.org/5595
    Reviewed-by: Aliaksey Kandratsenka <alkondratenko@gmail.com>
    Tested-by: Aliaksey Kandratsenka <alkondratenko@gmail.com>

---
+++ src/js/app-ui-misc.js
@@ -0,0 +1,149 @@
+/**
+   Copyright 2011 Couchbase, Inc.
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+ **/
+;(function($){
+  $.fn.boolAttr = function(attr, value) {
+    var $this = $(this);
+    if (value) {
+      $this.attr(attr, attr);
+    } else {
+      $this.removeAttr(attr);
+    }
+    return $this;
+  };
+
+  /**
+   * Combobox widget
+   * @link http://jqueryui.com/demos/autocomplete/#combobox
+   */
+  $.widget( "ui.combobox", {
+    _create: function() {
+      var self = this,
+          select = this.element.hide(),
+          selected = select.children( ":selected" ),
+          value = selected.val() ? selected.text() : "";
+      var input = this.input = $( "<input>" )
+        .insertAfter( select )
+        .val( value )
+        .autocomplete({
+          delay: 0,
+          minLength: 0,
+          source: function( request, response ) {
+            var matcher = new RegExp( $.ui.autocomplete.escapeRegex(request.term), "i" );
+            response( select.children( "option" ).map(function() {
+              var text = $( this ).text();
+              if ( this.value && ( !request.term || matcher.test(text) ) )
+                return {
+                  label: text.replace(
+                    new RegExp(
+                      "(?![^&;]+;)(?!<[^<>]*)(" +
+                      $.ui.autocomplete.escapeRegex(request.term) +
+                      ")(?![^<>]*>)(?![^&;]+;)", "gi"
+                    ), "<strong>$1</strong>" ),
+                  value: text,
+                  option: this
+                };
+            }) );
+          },
+          select: function( event, ui ) {
+            ui.item.option.selected = true;
+            self._trigger( "selected", event, {
+              item: ui.item.option
+            });
+            select.trigger('change');
+          },
+          change: function( event, ui ) {
+            if ( !ui.item ) {
+              var matcher = new RegExp( "^" + $.ui.autocomplete.escapeRegex( $(this).val() ) + "$", "i" ),
+                  valid = false;
+              select.children( "option" ).each(function() {
+                if ( $( this ).text().match( matcher ) ) {
+                  this.selected = valid = true;
+                  return false;
+                }
+              });
+              if ( !valid ) {
+                // remove invalid value, as it didn't match anything
+                $( this ).val( "" );
+                select.val( "" );
+                input.data( "autocomplete" ).term = "";
+                $(this).trigger('focus');
+                self.button.trigger('click');
+                return false;
+              }
+            }
+          }
+        })
+        .addClass( "ui-widget ui-widget-content ui-corner-left" );
+
+      input.click(function() {
+          $(this).select();
+        })
+        .keydown(function(ev) {
+          var visible = $(this).autocomplete('widget').find('li:visible');
+          // catch tab and enter keys
+          if (ev.which == 9 || ev.which == 13) {
+            if (visible.length == 1) {
+              $(this).val($(visible[0]).text())
+                .prev('select').find('option:contains('+$(visible[0]).text()+')')
+                .attr('selected', 'selected').trigger('change');
+            } else if (visible.length > 1) {
+              ev.preventDefault();
+            }
+            if (ev.which == 13) {
+              $(this).blur();
+            }
+          }
+        })
+        .data( "autocomplete" )._renderItem = function( ul, item ) {
+          return $( "<li></li>" )
+            .data( "item.autocomplete", item )
+            .append( "<a>" + item.label + "</a>" )
+            .appendTo( ul );
+        };
+
+      this.button = $( "<button type='button'>&nbsp;</button>" )
+        .attr( "tabIndex", -1 )
+        .attr( "title", "Show All Items" )
+        .insertAfter( input )
+        .button({
+          icons: {
+            primary: "ui-icon-triangle-1-s"
+          },
+          text: false
+        })
+        .removeClass( "ui-corner-all" )
+        .addClass( "ui-corner-right ui-button-icon" )
+        .click(function() {
+          // close if already visible
+          if ( input.autocomplete( "widget" ).is( ":visible" ) ) {
+            input.autocomplete( "close" );
+            return;
+          }
+
+          // pass empty string as value to search for, displaying all results
+          input.autocomplete( "search", "" );
+          input.focus();
+        });
+    },
+
+    destroy: function() {
+      this.input.remove();
+      this.button.remove();
+      this.element.show();
+      $.Widget.prototype.destroy.call( this );
+    }
+  });
+})(jQuery);
